4e37da6eb9e226bc256cc44b0d54d5bdb86910c3
Sajanpreet35 <sajansingh2037@gmail.com>
2025-12-18 19:39:04 +0530

    Create relief.js

---
+++ relief.js
@@ -0,0 +1,8 @@
+export async function relief(country){
+  return `
+    <h2>🚨 ReliefWeb – Crises</h2>
+    <div class="card">
+      Disaster & humanitarian updates for ${country}
+    </div>
+  `
+}
