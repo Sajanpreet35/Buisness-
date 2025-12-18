2654e958d07625c764cc92aa193ac56202cfc886
Sajanpreet35 <sajansingh2037@gmail.com>
2025-12-18 19:38:38 +0530

    Create acled.js

---
+++ acled.js
@@ -0,0 +1,8 @@
+export async function acled(country){
+  return `
+    <h2>⚠ ACLED – Conflict & Deaths</h2>
+    <div class="card">
+      Weekly incidents in ${country} reported
+    </div>
+  `
+}
